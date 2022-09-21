2a80dc9ea15e763591966158cb57466f3ff0f285
LAPTOP-1UIQE60B\OperThirteen <oprthrtn@gmail.com>
2022-09-21 21:53:54 +0700

    Реализован запрос на обновление кулинарного рецепта.

---
+++ routers/updateRecipe.php
@@ -0,0 +1,74 @@
+<?php
+
+function checkAuthor($currentNickname, $id, $Link)
+{
+    $query = "SELECT AuthorNickname FROM recipes WHERE ID = ?";
+    $stmt = $Link->prepare($query);
+    $stmt->bind_param("i", intval($id));
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($AuthorNickname);
+    $stmt->fetch();
+
+    echo json_encode([$currentNickname, $AuthorNickname]);
+    if ($currentNickname === $AuthorNickname) {
+        return true;
+    }
+
+    return false;
+}
+
+function route($method, $urlList, $requestData)
+{
+    global $Link;
+    if ($method === 'PATCH' && count($urlList) === 1 && gettype($urlList[0] === "integer")) {
+        $authHeader = apache_request_headers()['Authorization'];
+        $token = explode(' ', $authHeader)[1];
+
+
+        $query = "SELECT Nickname FROM authorizedusers WHERE token = ?";
+        $stmt = $Link->prepare($query);
+        $stmt->bind_param("s", $token);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($currentNickname);
+        $stmt->fetch();
+
+        if ($currentNickname) {
+
+            $title = $requestData->body->title;
+            $description = $requestData->body->description;
+            $cookTime = $requestData->body->cookTime;
+            $direction = $requestData->body->direction;
+            $imageURL = $requestData->body->imageURL;
+
+            $query = "SELECT RoleName FROM users WHERE Nickname = ?";
+            $stmt = $Link->prepare($query);
+            $stmt->bind_param("s", $currentNickname);
+            $stmt->execute();
+            $stmt->store_result();
+            $stmt->bind_result($currentRole);
+            $stmt->fetch();
+
+            echo json_encode([$currentNickname, $currentRole]);
+            if ($currentRole === "admin") {
+                $query = "UPDATE `recipes` SET `Title`=?,`Description`=?,`CookTime`=?,`Direction`=?,`ImageURL`=? WHERE `ID`=?";
+
+                $stmt = $Link->prepare($query);
+                $stmt->bind_param("ssisss", $title, $description, $cookTime, $direction, $imageURL, $urlList[0]);
+                $stmt->execute();
+            } else if ($currentRole === "user" && checkAuthor($currentNickname, $urlList[0],  $Link)) {
+                $query = "UPDATE `recipes` SET `Title`=?,`Description`=?,`CookTime`=?,`Direction`=?,`ImageURL`=? WHERE `ID`=?";
+
+                $stmt = $Link->prepare($query);
+                $stmt->bind_param("ssisss", $title, $description, $cookTime, $direction, $imageURL, $urlList[0]);
+                $stmt->execute();
+            } else {
+                setHTTPStatus("403", 'Вы можете обновить только свои рецепты');
+                return;
+            }
+        } else {
+            setHTTPStatus("403", 'Вы не авторизованы.');
+        }
+    }
+}
